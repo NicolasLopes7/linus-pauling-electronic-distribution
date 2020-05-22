0090dfcc059ec3ac4ba92cfe8a6243992e0b9ccc
Nicolas Lopes Aquino <nicolaslopesbusiness@gmail.com>
2020-05-22 14:16:14 -0300

    Adding guide for the distribution

---
+++ src/config/guide.js
@@ -0,0 +1,23 @@
+const guide = [
+  { conf: 2, name: "1S", lim: 0 },
+  { conf: 2, name: "2S", lim: 0 },
+  { conf: 6, name: "2P", lim: 0 },
+  { conf: 2, name: "3S", lim: 0 },
+  { conf: 6, name: "3P", lim: 0 },
+  { conf: 2, name: "4S", lim: 0 },
+  { conf: 10, name: "3D", lim: 0 },
+  { conf: 6, name: "4P", lim: 0 },
+  { conf: 2, name: "5S", lim: 0 },
+  { conf: 10, name: "4D", lim: 0 },
+  { conf: 6, name: "5P", lim: 0 },
+  { conf: 2, name: "6D", lim: 0 },
+  { conf: 14, name: "4F", lim: 0 },
+  { conf: 10, name: "5D", lim: 0 },
+  { conf: 6, name: "6P", lim: 0 },
+  { conf: 2, name: "7S", lim: 0 },
+  { conf: 14, name: "5F", lim: 0 },
+  { conf: 10, name: "6D", lim: 0 },
+  { conf: 6, name: "7P", lim: 0 },
+];
+
+module.exports = guide;
